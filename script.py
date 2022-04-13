9dea645d4fd09af67f4558fd92e8e2c1d1634efb
gbloom2010 <95660807+gbloom2010@users.noreply.github.com>
2022-04-12 20:33:35 -0600

    Update script.py
    modified logic for playing game and enhanced readability of code

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 import random
-from unicodedata import name
 
 class Card:
     
@@ -16,7 +15,7 @@ class Card:
 class Deck:
 
     def __init__(self):
-        pot = 0
+        self.pot = 0
         self.mainDeck = []
         for suit in Card.suits:
             for face in Card.faces.keys():
@@ -33,26 +32,31 @@ class Deck:
         self.mainDeck.remove(drawnCard)
         return drawnCard
 
+    def report_win(self, player, dealer):
+        player_win = player.sum > dealer.sum and player.sum <= 21
+        if player_win or dealer.sum > 21:
+            return "{name} has won!".format(name=player.name)
+        elif not player_win and player.sum == dealer.sum:
+            player.money += player.bet
+            dealer.money += dealer.bet
+            return "{name} has tied with the Dealer!".format(name = player.name)
+        return "The Dealer beat {name}!".format(name = player.name)
+
     def start_round(self, player, dealer):
-        player.betMoney()
-        dealer.betMoney()
+        deck.takeBet(player)
+        deck.takeBet(dealer)
         for i in range(2):
             player.draw_card(self)
             dealer.draw_card(self)
         print("Dealer has a {card}".format(card=dealer.hand[-1]))
 
-        while(not player.play_round(dealer, deck)):
+        while(not player.decide(dealer, deck)):
             pass
 
-        while(not dealer.play_round(player, deck)):
+        while(not dealer.decide(player, deck)):
             pass
-        # player.
 
-    def is_win(self, other):
-        win = self.sum > other.sum and self.sum <= 21
-        if win:
-            print("{name} with {sum} beat ")
-        return win
+        print(self.report_win(player, dealer))
 
 class Player:
 
@@ -62,36 +66,55 @@ class Player:
         self.money = 3000
         self.sum = 0
         self.ai = ai
+        self.bet = 50
+        self.busted = False
     
     def __repr__(self):
         return "{name}, you have {money} left to play.".format(name = self.name, money = self.money)
 
     def betMoney(self):
-        bet = 30
-        self.money -= bet
-        return bet
+        
+        self.money -= self.bet
+        return self.bet
 
     def draw_card(self, deck):
         card = deck.drawCard()
         self.hand.append(card)
         self.sum += Card.faces[card.face]
 
-    def play_round(self, other, deck):
+    def hit(self, deck):
+        print("{name} has {sum}, they hit.".format(name=self.name, sum = self.sum))
+        self.draw_card(deck)
         if self.sum > 21:
+            self.busted = True
             print("{name} has {sum}, they have busted.".format(name=self.name, sum = self.sum))
             return True
-        elif self.sum < 18 and self.sum < other.sum:
-            print("{name} has {sum}, they hit.".format(name=self.name, sum = self.sum))
-            self.draw_card(deck)
-            print("{name} now has {sum}".format(name=self.name, sum = self.sum))
-            return False
+        print("{name} now has {sum}".format(name=self.name, sum = self.sum))
+        return False
+
+    def stand(self):
         print("{name} has {sum}, they decide to stand.".format(name=self.name, sum = self.sum))
         return True
 
+    def fold(self):
+        print("{name} has decided to fold.".format(name=self.name))
+        return True
+
+    def decide(self, other, deck):
+        if not other.busted:
+            # if self.ai:
+            if self.sum <= 16 or not (self.sum >= other.sum):
+                return self.hit(deck)
+            elif self.sum in range(18, 22):
+                return self.stand()
+        return self.stand()
+
     def reset(self, deck):
         deck.mainDeck += self.hand
-        self.__init__(self.name, self.ai)
-        pass
+        self.hand = []
+        self.sum = 0
+        self.busted = False
+
 
 deck = Deck()
 player = Player("Greg")
