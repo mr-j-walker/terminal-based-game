82a76cf505b51c5cb3f87c8440c3c2599ac6c9d0
[gbloom3] <95660807+gbloom2010@users.noreply.github.com>
2022-04-17 14:54:23 -0600

    Update script.py
    reformatted code and adjusted logic

--- script.py
+++ script.py
@@ -1,16 +1,19 @@
 import random
 
+
 class Card:
-    
+
     suits = ["clubs", "spades", "hearts", "diamonds"]
-    faces = {"ace": 1, "two" : 2, "three" : 3, "four" : 4, "five" : 5, "six" : 6, "seven" : 7, "eight" : 8, "nine": 9, "ten" : 10 , "jack" : 10, "queen" : 10, "king" : 10}
+    faces = {"ace": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6,
+             "seven": 7, "eight": 8, "nine": 9, "ten": 10, "jack": 10, "queen": 10, "king": 10}
 
     def __init__(self, suit, face):
         self.suit = suit
         self.face = face
 
     def __repr__(self):
-        return "{face} of {suit}".format(face = self.face.title(), suit = self.suit.title())
+        return "{face} of {suit}".format(face=self.face.title(), suit=self.suit.title())
+
 
 class Deck:
 
@@ -23,91 +26,114 @@ class Deck:
 
     def __repr__(self):
         return "There are {} cards left in the deck.".format(len(self.mainDeck))
-    
-    def takeBet(self, player):
-        self.pot += player.betMoney()
-    
+
     def drawCard(self):
         drawnCard = random.choice(self.mainDeck)
         self.mainDeck.remove(drawnCard)
         return drawnCard
 
-    def report_win(self, player, dealer):
-        player_win = player.sum > dealer.sum and player.sum <= 21
-        if player_win or dealer.sum > 21:
-            return "{name} has won!".format(name=player.name)
-        elif not player_win and player.sum == dealer.sum:
-            player.money += player.bet
-            dealer.money += dealer.bet
-            return "{name} has tied with the Dealer!".format(name = player.name)
-        return "The Dealer beat {name}!".format(name = player.name)
-
-    def start_round(self, player, dealer):
-        deck.takeBet(player)
-        deck.takeBet(dealer)
+    def dealCards(self, player, dealer):
         for i in range(2):
-            player.draw_card(self)
-            dealer.draw_card(self)
+            player.drawCard(self)
+            dealer.drawCard(self)
+
+    def reportWin(self, player, dealer):
+        player_win = player.sum > dealer.sum and player.sum <= 21
+        dealer_win = (player.sum < dealer.sum or player.sum > 21) and dealer.sum <= 21
+        if player_win and not dealer_win:
+            return "{name} has won!".format(name=player.name)
+        elif dealer_win and not player_win:
+            return "The Dealer beat {name}!".format(name=player.name)
+        return "{name} has tied with the Dealer!".format(name=player.name)
+
+
+    def playRound(self, player, dealer):
+        self.dealCards(player, dealer)
         print("Dealer has a {card}".format(card=dealer.hand[-1]))
 
-        while(not player.decide(dealer, deck)):
+        while(player.control(dealer, deck)):
             pass
 
-        while(not dealer.decide(player, deck)):
+        while(dealer.decide(player, deck)):
             pass
 
-        print(self.report_win(player, dealer))
+        print(self.reportWin(player, dealer))
+
 
 class Player:
 
-    def __init__(self, name, ai = False):   
+    def __init__(self, name, ai=False):
         self.name = name
         self.hand = []
-        self.money = 3000
         self.sum = 0
         self.ai = ai
-        self.bet = 50
         self.busted = False
-    
+
     def __repr__(self):
-        return "{name}, you have {money} left to play.".format(name = self.name, money = self.money)
+        return "{name}, you have {count} left to play.".format(name=self.name, count=len(self.hand))
 
-    def betMoney(self):
-        
-        self.money -= self.bet
-        return self.bet
-
-    def draw_card(self, deck):
+    def drawCard(self, deck):
         card = deck.drawCard()
         self.hand.append(card)
         self.sum += Card.faces[card.face]
 
     def hit(self, deck):
-        print("{name} has {sum}, they hit.".format(name=self.name, sum = self.sum))
-        self.draw_card(deck)
+        print("{name} has {sum}, they hit.".format(
+            name=self.name, sum=self.sum))
+        self.drawCard(deck)
         if self.sum > 21:
             self.busted = True
-            print("{name} has {sum}, they have busted.".format(name=self.name, sum = self.sum))
-            return True
-        print("{name} now has {sum}".format(name=self.name, sum = self.sum))
-        return False
+            print("{name} has {sum}, they have busted.".format(
+                name=self.name, sum=self.sum))
+            return False
+        print("{name} now has {sum}".format(name=self.name, sum=self.sum))
+        return True
 
     def stand(self):
-        print("{name} has {sum}, they decide to stand.".format(name=self.name, sum = self.sum))
-        return True
+        print("{name} has {sum}, they decide to stand.".format(
+            name=self.name, sum=self.sum))
+        return False
 
     def fold(self):
         print("{name} has decided to fold.".format(name=self.name))
-        return True
+        return False
+
+    def control(self, other, deck):
+        if self.ai:
+            return self.decide(other, deck)
+        else:
+            return self.choose(deck)
+
+    def choose(self, deck):
+        prompt = """
+        How would you like to play?
+        1 - Hit
+        2 - Stand
+        3 - Fold (1/2 of your bet is returned)\n
+        """
+        print("Your Hand:\n")
+        for card in self.hand:
+            print(card)
+        if not self.busted:
+            pick = input(prompt)
+            if pick == "1":
+                return self.hit(deck)
+            elif pick == "2":
+                return self.stand()
+            elif pick == "3":
+                return self.fold()
+            else:
+                print("Invalid Input!")
+                return self.choose(Deck)
+        return False
 
     def decide(self, other, deck):
         if not other.busted:
-            # if self.ai:
             if self.sum <= 16 or not (self.sum >= other.sum):
                 return self.hit(deck)
             elif self.sum in range(18, 22):
                 return self.stand()
-        return self.stand()
+        return False
 
     def reset(self, deck):
         deck.mainDeck += self.hand
@@ -117,7 +143,9 @@ class Player:
 
 
 deck = Deck()
-player = Player("Greg")
+player = Player(" ")
 dealer = Player("The Dealer", True)
 
-deck.start_round(player, dealer)
+player.name = input("What is your name?\n")
+
+deck.playRound(player, dealer)
